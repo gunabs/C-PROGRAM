80e70b6a978be0d605496812dfcdad129cb04df9
Guna.B <114652160+gunabs@users.noreply.github.com>
2025-11-06 13:28:15 +0530

    Power of Four

---
+++ LEEDCODE/leedcode-5.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main() {
+   int n=16;
+   unsigned int mul=4;
+   
+   while(mul<=n)
+   {
+      mul=mul*4;
+   
+   if(n==mul)
+   {
+       printf("yes");
+   }
+   
+}
+    return 0;
+}
