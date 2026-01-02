a58a75748065ad7288334045d87487f5a1887acc
Guna.B <114652160+gunabs@users.noreply.github.com>
2026-01-02 14:08:11 +0530

    Fibonacci Number

---
+++ LEEDCODE/leedcode-18.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int fib(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    int a = 0, b = 1, c;
+
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
+}
+int main ()
+{
+  int result=fib(4);
+  printf("%d",result);
+}
